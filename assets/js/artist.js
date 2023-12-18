908c1570e6756e7f8db45d29cd8414c36bd081af
Dagniele24 <danielelazzarano@gmail.com>
2023-12-18 19:16:42 +0100

    asdasd

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,9 +1,8 @@
-
 function urlGeneratorArtist(id) {
     return urlArtist + id;
   }
 
-  const getRecord = (id) => {
+  const getArtist = (id) => {
     fetch(urlGeneratorArtist(id), {
       headers: {
         authorization: token,
@@ -14,21 +13,17 @@ function urlGeneratorArtist(id) {
       .then((data) => {
         artists.push(data);
         // tracks.push(data.tracks);
-        createArtistSection(data);
+        createBanner(data);
       });
   };
 
-  function createArtistSection(data) {
+  function createBanner(data) {
     console.log(data);
     const containerTrack = document.querySelector('#container-track2');
-  containerTrack.innerHTML = `
-
- 
-
-  
-  <div class=" mx-0" style="background-image: url(${data.picture_xl}); background-size: cover ; background-position: 50% 40%; " >
-  <div >
-  <div
+    containerTrack.innerHTML = `
+    <div class=" mx-0" style="background-image: url(${data.picture_xl}); background-size: cover ; background-position: 50% 40%; " >
+    <div >
+    <div
                         class=" p-2 pb-5 row-container track-management mb-2 d-flex flex-row justify-content-between align-items-center">
                         <div class="track-control-icons mx-3">
                             <a href="index.html">
@@ -60,8 +55,8 @@ function urlGeneratorArtist(id) {
   <h5 class="pt-5 text-white"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill=" skyblue" class="bi bi-patch-check-fill text-white" viewBox="0 0 16 16">
   <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
 </svg>   artista verificato</h5>
-    <h1 class="text-white display-2  "> <b> ${data.name} </b> </h1>
-    <h5 class="text-white py-4">${data.nb_fan} ascoltatori mensili</h5>
+    <h1 class="text-white display-2"> <b> ${data.name} </b> </h1>
+    <h5 class="text-white py-4 shadow-lg">${data.nb_fan} ascoltatori mensili</h5>
  </div>
 </div>
 
@@ -71,5 +66,5 @@ function urlGeneratorArtist(id) {
   window.onload = () => {
     const params = new URLSearchParams(location.search);
     const id = params.get('id');
-    getRecord(id);
+    getArtist(id);
   };
