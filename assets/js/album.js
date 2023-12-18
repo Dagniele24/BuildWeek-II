14e59631b0e1972d5f9025fcb58ead2e0e2cf926
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-18 13:54:34 +0100

    fix html e funzione conversione durata canzoni singole

--- assets/js/album.js
+++ assets/js/album.js
@@ -6,52 +6,54 @@ const getRecord = (id) => {
   fetch(urlGeneratorAlbum(id), {
     headers: {
       authorization: token,
-      Accept: "application/json",
+      Accept: 'application/json',
     },
   })
     .then((response) => response.json())
     .then((data) => {
       albums.push(data);
-     // tracks.push(data.tracks);
+      // tracks.push(data.tracks);
 
       createAlbumSection(data);
       console.log(data.tracks.data);
-      createTable(data.tracks.data)
+      createTable(data.tracks.data);
       //createTable(data.tracks)
-      
     });
 };
-
 
 function createAlbumSection(data) {
   //RECUPERO ANNO ALBUM
   let date = data.release_date;
   let year = date.split('-')[0];
-   /* let year = date.slice(0, 4); */
-  
+  /* let year = date.slice(0, 4); */
+
   // RECUPERO DURATA ALBUM
-    convertTime(data);
-  
-  const containerTrack = document.querySelector("#container-track2");
+  convertTime(data);
+
+  const containerTrack = document.querySelector('#container-track2');
   containerTrack.innerHTML = `
 
     // ///
     <div class="card mb-3 mt-5 border-0 bg-transparent text-white container" style="max-width: 1000px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="${data.cover_medium}" class="img-fluid rounded-start" alt="${data.title}">
+      <img src="${data.cover_medium}" class="img-fluid rounded-start" alt="${
+    data.title
+  }">
     </div>
     <div class="col-md-8">
      <div class="row">
       <div class="card-body  mt-5">
         <h1 class="card-title">${data.title}</h1>
         <h4 id="artist-name">${data.artist.name}</h4>
-        <div class="container   mt-5 align-items-center "><div class=" row d-flex align-items-center"><img class="rounded-circle col-1" src="${data.artist.picture} ">
+        <div class="container   mt-5 align-items-center "><div class=" row d-flex align-items-center"><img class="rounded-circle col-1" src="${
+          data.artist.picture
+        } ">
         <p class="col-11  mt-3">
         <span class="">${data.artist.name}</span>
         <span class="">- ${year}</span>
         <span class="">-${data.nb_tracks} brani,</span>
-        <span class="text-secondary">${min +  ' min. ' + sec +  ' sec.'}</span>
+        <span class="text-secondary">${min + ' min. ' + sec + ' sec.'}</span>
         </p>
         </div>
         </div>
@@ -65,29 +67,31 @@ function createAlbumSection(data) {
 </div>
 
     `;
-  
+}
+
+// Funzione conversione durata album
+
+function convertTime(data) {
+  let songDuration = data.duration;
+  min = Math.floor(songDuration / 60);
+  sec = Math.floor(songDuration % 60);
+}
+
+// Funzione conversione durata singola canzone
+
+function convertTimeAlbums(durationInSeconds) {
+  const minutes = Math.floor(durationInSeconds / 60);
+  const seconds = durationInSeconds % 60;
+  return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
 }
 
 //FUNZIONE POPOLA LISTA PAGINA ALBUM
 
-function convertTime(data) {
-let songDuration = data.duration; 
-min = Math.floor(songDuration/60);
-sec = Math.floor(songDuration % 60);
-}
-
-function convertTimeAlbums(data) {
-  let songDuration = data.tracks.data; 
-  min = Math.floor(songDuration/60);
-  sec = Math.floor(songDuration % 60);
-}
 function createTable(tracks) {
-  convertTimeAlbums(track.duration);
-  const tBody = document.querySelector("#bodyTable");
-  let tableHTML = ""
+  const tBody = document.querySelector('#bodyTable');
+  let tableHTML = '';
   tracks.forEach((track, index) => {
-    tableHTML +=
-    `
+    tableHTML += `
   <tr>
   <div id="margine">
     <th scope="row" class="bg-transparent">${index + 1}</th>
@@ -98,23 +102,19 @@ function createTable(tracks) {
     </div>
     </td>
 
-    
     <td>${track.rank}</td>
   
-   
-    <td >${convertTimeAlbums(track.duration)} + sec</td>
+    <td >${convertTimeAlbums(track.duration)}</td>
    
   </tr>
 
   `;
-
-  })
-  tBody.innerHTML = tableHTML
+  });
+  tBody.innerHTML = tableHTML;
 }
 
-
-  window.onload = () => {
-    const params = new URLSearchParams(location.search);
-    const id = params.get("id");
-    getRecord(id);
-  };
+window.onload = () => {
+  const params = new URLSearchParams(location.search);
+  const id = params.get('id');
+  getRecord(id);
+};
