67652ed5c2f27a3ce04a7a299cce0005c031a4ad
Dagniele24 <danielelazzarano@gmail.com>
2023-12-19 11:02:16 +0100

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,13 +143,13 @@ function createFooter() {
   </div>`;
 }
 
+
 const cuoreIcon = document.querySelector('.cuore');
 cuoreIcon.onclick = () => {
   const currentColor = cuoreIcon.getAttribute('fill');
   const newColor = currentColor === 'green' ? '' : 'green';
   cuoreIcon.setAttribute('fill', newColor);
 };
-
 window.onload = () => {
   getAlbums();
   getArtists();
