3adbf6651d1186d89929ba2377dc1a943882b8f7
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-19 15:39:57 +0100

    fix html e inizio generazione cuore sul footer

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -147,6 +147,19 @@ function createFooter(trackIndex) {
   </p>
 </div>
 </div>`;
+  createHeart(footerSong);
+}
+
+function createHeart(footerSong) {
+  const heartSection = document.querySelector('.heart-section');
+  if (footerSong.innerHTML !== '') {
+    heartSection.innerHTML = `<svg
+xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill=""
+class="bi bi-heart-fill cuore" viewBox="0 0 16 16">
+<path fill-rule="evenodd"
+    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+</svg>`;
+  }
 }
 
 window.onload = () => {
